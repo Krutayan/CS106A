5503a5eaeb2917777a87b8c036cfa4571851049d
Krutayan Pandey <krutayanpandey@gmail.com>
2026-06-13 17:14:45 +0530

    Create Zebra_Crossing.py
    In this program, Karel draws a Zebra crossing pattern. The code is dynamically scalable and perfectly valid for any world with dimensions (5a + 2) x N, where a >= 0 represents the number of complete crossing cycles, and N >= 1 represents the height of the matrix.

---
+++ Assignments/Zebra_Crossing.py
@@ -0,0 +1,47 @@
+from karel.stanfordkarel import *
+
+def main():
+    while front_is_clear():
+        paint_column()
+        if front_is_clear():
+            pass_3unit()
+
+def paint_column():
+    # pre : facing east at bottom row
+    #post : facing east at bottom row displaced by 1 X Co-ord units
+    turn_left()
+    while front_is_clear():
+        put_beeper()
+        move()
+    put_beeper()  # fench post problem
+    change_column()
+    while front_is_clear():
+        put_beeper()
+        move()
+    put_beeper() # fench post promblem
+    turn_left()
+
+def pass_3unit():
+    # karel displaced by 4 X Co-Ord units
+    if front_is_clear():
+        for i in range (4):
+            move()
+
+def change_column():
+    turn_right()
+    move()
+    turn_right()
+
+def turn_right():
+    for i in range (3):
+        turn_left()
+
+
+
+
+
+
+# There is no need to edit code beyond this point
+
+if __name__ == '__main__':
+    main()
