1a6de35ee0e2393cf001ad3d28668d9ab9ca20e6
Krutayan Pandey <krutayanpandey@gmail.com>
2026-06-14 13:30:51 +0530

    Create favorite_animal.py
    In this we take input from use by asking "What's your favorite animal? " and take that input and using that input to print

---
+++ Intro_to_python/Core_Lessons/favorite_animal.py
@@ -0,0 +1,9 @@
+def main():
+    animal = input("What's your favorite animal? ")
+    print(f"My favorite animal is also {animal}!")
+
+
+# There is no need to edit code beyond this point
+
+if __name__ == '__main__':
+    main()
