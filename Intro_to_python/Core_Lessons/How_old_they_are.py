8e394fb613531f42a80b77dfd8365f80535e656e
Krutayan Pandey <krutayanpandey@gmail.com>
2026-06-20 22:05:12 +0530

    Create How_old_they_are.py
    in this i solved basic age related puzzle using basin arithematic logic in python

---
+++ Intro_to_python/Core_Lessons/How_old_they_are.py
@@ -0,0 +1,23 @@
+"""
+This is a worked example. This code is starter code; you should edit and run it to 
+solve the problem. You can click the blue show solution button on the left to see 
+the answer if you get too stuck or want to check your work!
+"""
+
+def main():
+    Anton_age = 21
+    Beth_age = 6 + Anton_age
+    Chen_age = 20 + Beth_age
+    Drew_age = Chen_age + Anton_age
+    Ethan_age = Chen_age
+    print("Anton is", Anton_age)
+    print("Beth is", Beth_age)
+    print("Chen is", Chen_age)
+    print("Drew is", Drew_age)
+    print("Ethan is", Ethan_age)
+
+
+# There is no need to edit code beyond this point
+
+if __name__ == '__main__':
+    main()
